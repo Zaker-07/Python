b1b24aa818cbe42b57a16e7326c0df2d595b7a2c
Zaker-07 <109406340+Zaker-07@users.noreply.github.com>
2025-08-18 18:42:42 +0530

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,10 @@
+x=int(input("Enter the first number: "))
+y=int(input("Enter the second number: "))
+a=x+y
+b=x-y
+c=x*y
+d=x/y
+print("Addition of two numbers is: ",a)
+print("Substraction of two numbers is: ",b)
+print("Multiplication of two numbers is: ",c)
+print("Division of two numbers is: ",d)
